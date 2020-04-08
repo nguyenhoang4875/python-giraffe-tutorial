494d16da53af66591525868f41ccfabf5cc10686
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-04-08 11:10:50 +0700

    variables & data type

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello world!")
+character_name = "John"
+character_age = "50"
+is_male = False
+
+print("There once was a man named " + character_name + ",")
+print("he was " + character_age + " years old. ")
+character_name = "Mike"
+print("He really liked the name  " + character_name + ",")
+print("but didn't like being " + character_age + " years")
