9140d39b9a57595fa99440badc201d291184cc60
nguyenhoang4875 <contraicuagio97@gmail.com>
2020-04-16 14:34:47 +0700

    for loops

--- app.py
+++ app.py
@@ -1,19 +1,12 @@
-secret_word = "giraffe"
-guess = ""
-guess_count = 0
-guess_limit = 3
-out_of_guesses = False
+friends = ["Jim", "Karen", "Kevin"]
+for index in friends:
+    print(index)
 
-while guess != secret_word and  not(out_of_guesses):
-    if guess_count < guess_limit:
-        guess = input("Enter guess: ")
-        guess_count +=1
-    else: 
-        out_of_guesses = True
+for index in range(1,10):
+    print(index)
 
-
-
-if out_of_guesses:    
-    print("Out of Guesses, You Lose")
-else:
-    print("You win!")
+for index in range(5):
+    if index == 0:
+        print("first Iteration")
+    else:
+        print("Not first")
